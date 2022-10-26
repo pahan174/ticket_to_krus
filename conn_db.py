6e6be7c0d926542e8307b266558c6ba09a5c5d03
Pavel U <pahan174@gmail.com>
2022-10-26 11:24:17 +0500

    test request

--- conn_db.py
+++ conn_db.py
@@ -7,11 +7,19 @@ load_dotenv()
 un = os.getenv('PYTHON_USERNAME')
 pw = os.getenv('PYTHON_PASSWORD')
 cs = os.getenv('PYTHON_CONNECTSTRING')
+deveui='5CA05DEF6B070B04'
 
 oracledb.init_oracle_client()
 
 with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
     with connection.cursor() as cursor:
-        sql = """select sysdate from dual"""
-        for r in cursor.execute(sql):
+        req = '''
+        BEGIN
+        :result := tt.uni_bss.find_device_for_zabbix(p_deveui => '5CA05DEF6B070B04',
+        p_tt_trouble => 'Проверка');
+        commit;
+        end;
+        '''
+        # sql = """select sysdate from dual"""
+        for r in cursor.execute(req):
             print(r)
