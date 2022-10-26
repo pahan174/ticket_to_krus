2d5b5fcb905c327f66e19a217b70128db1094f1a
Pavel U <pahan174@gmail.com>
2022-10-26 14:53:06 +0500

    test with zabbix

--- conn_db.py
+++ conn_db.py
@@ -2,7 +2,7 @@ import os
 import oracledb
 from dotenv import load_dotenv
 import sys
-
+from pyzabbix import ZabbixAPI
 
 
 load_dotenv()
@@ -10,15 +10,26 @@ load_dotenv()
 un = os.getenv('PYTHON_USERNAME')
 pw = os.getenv('PYTHON_PASSWORD')
 cs = os.getenv('PYTHON_CONNECTSTRING')
+URL_ZABBIX = os.getenv("URL_ZABBIX", default='127.0.0.1')
+USER_ZABBIX = os.getenv("USER_ZABBIX", default='Admin')
+USER_PASS = os.getenv("USER_PASS", default='Admin')
+
 deveui = sys.argv[1]
 text = sys.argv[2]
 eventid = sys.argv[3]
 
-print(deveui)
-print(text)
-print(eventid)
+
+def send_event_info(zapi, text, action) -> None:
+    zapi.do_request('event.acknowledge',
+                    {
+                     'eventids': eventid,
+                     'action': action,
+                     'message': text
+                     })
+
 
 oracledb.init_oracle_client()
+zapi = ZabbixAPI(url=URL_ZABBIX, user=USER_ZABBIX, password=USER_PASS)
 try:
     with oracledb.connect(user=un, password=pw, dsn=cs) as connection:
         with connection.cursor() as cursor:
@@ -37,9 +48,9 @@ try:
                     'result': result,
                 })
 except Exception as e:
-    print(f'Ошибка {e}')
+    send_event_info(zapi, f'Нет связи с БД КРУС. Ошибка {e}', 4)
 else:
     if 'no data found' in result.getvalue():
-        print('Такого DEVEUI нет в КРУС')
+        send_event_info(zapi, 'Устройства нет в КРУС', 6)
     else:
-        print(f'ОТкрыт тикет {result.getvalue()}')
+        send_event_info(zapi, f'Номер тикета {result.getvalue()}', 6)
